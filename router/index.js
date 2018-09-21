3e69eebb0aac4bac65fae4836492626f5b3db669
AlanGuo <AlanGuo>
2018-09-21 16:45:10 +0800

    update

--- router/index.js
+++ router/index.js
@@ -15,9 +15,9 @@ module.exports = function(routers) {
         return ctx.request.body.address + ctx.request.header["x-real-ip"]
       },
       headers: {
-          remaining: 'Rate-Limit-Remaining',
-          reset: 'Rate-Limit-Reset',
-          total: 'Rate-Limit-Total'
+        remaining: 'Rate-Limit-Remaining',
+        reset: 'Rate-Limit-Reset',
+        total: 'Rate-Limit-Total'
       },
       max: 1,
       disableHeader: false,
@@ -31,9 +31,9 @@ module.exports = function(routers) {
       return ctx.request.header["x-real-ip"]
     },
     headers: {
-        remaining: 'Ip-Limit-Remaining',
-        reset: 'Ip-Limit-Reset',
-        total: 'Ip-Limit-Total'
+      remaining: 'Ip-Limit-Remaining',
+      reset: 'Ip-Limit-Reset',
+      total: 'Ip-Limit-Total'
     },
     max: 6,
     disableHeader: false,
