519e31fef92d40c14a6eef49f381dc4ed1c0cd9b
AlanGuo <AlanGuo>
2018-09-21 16:37:03 +0800

    update

--- router/index.js
+++ router/index.js
@@ -19,7 +19,7 @@ module.exports = function(routers) {
           reset: 'Rate-Limit-Reset',
           total: 'Rate-Limit-Total'
       },
-      max: 1,
+      max: 3,
       disableHeader: false,
   })
 
