7900302c7416dce210a515bf50c2c744e3b15050
AlanGuo <AlanGuo>
2018-11-17 15:43:21 +0800

    update

--- services/controller/faucet.js
+++ services/controller/faucet.js
@@ -116,7 +116,7 @@ module.exports = {
       }
       const transaction = new bch.Transaction()
       .from(utxos)
-      .to(address, 0.00001 * 1e8)
+      .to(address, 0.00005 * 1e8)
       .change(bchAbcFrom.address)
       .sign(bchAbcFrom.privKey)
 
